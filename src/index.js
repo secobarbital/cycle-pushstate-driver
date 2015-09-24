3c509724ccd25d73ff566d45707073f5df838485
Seggy Umboh <seg@seg.gy>
2015-09-24 00:01:32 -0700

    Use subscribe to avoid double action cyclejs/cycle-core#169

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ export function makeURLDriver () {
   return function URLDriver (navigate$) {
     const pushState$ = navigate$
       .distinctUntilChanged()
-      .do(url => history.pushState(null, null, url))
     const popState$ = Rx.Observable.fromEvent(global, 'popstate')
       .map(e => location.pathname)
+
+    pushState$
+      .subscribe(url => history.pushState(null, null, url))
+
     return Rx.Observable.merge(pushState$, popState$)
       .startWith(location.pathname)
       .distinctUntilChanged()
